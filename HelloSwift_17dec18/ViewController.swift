c4ec85b5833efe8d0dc874d08c3ffdcb3bef5c72
masterUNG <phrombutr@gmail.com>
2018-12-17 13:37:22 +0700

    Change and Back Success

--- HelloSwift_17dec18/ViewController.swift
+++ HelloSwift_17dec18/ViewController.swift
@@ -12,18 +12,35 @@ class ViewController: UIViewController {
     
 //    Explicit
     var titleString: String = "Master UNG"
+    let currentTitle: String = "This is Label"
+    let currentButton: String = "Change"
+    let backButton: String = "Back"
     
+    var status: Bool = false
+    
+    
+
+    @IBOutlet weak var changeOutlet: UIButton!
     
     
     @IBOutlet weak var titleLabel: UILabel!
     
     
-    
     @IBAction func changeButton(_ sender: Any) {
         
         print("You Click ChangeButton")
+        status = !status
         
-        titleLabel.text = titleString
+        if status {
+            titleLabel.text = titleString
+        } else {
+            titleLabel.text = currentTitle
+        }
+        
+        
+        
+        
+        
         
     }   // changeButton Functon
     
