6c1301fd1ca272f4402499debdcfbfa2b956a581
masterUNG <phrombutr@gmail.com>
2018-12-17 11:33:38 +0700

    Design UI Main Success

---
+++ HelloSwift_17dec18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSwift_17dec18
+//
+//  Created by MasterUNG on 17/12/2561 BE.
+//  Copyright © 2561 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }   // Main Function
+
+
+}   // Main Class
+
